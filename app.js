0dae8f3ddd913dd44c4d1517c473b977b698fce3
Anna Golubtsova <marickam@yandex.ru>
2024-01-15 15:56:48 +0300

    added links at README file

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const responseHandler = require('./middlewares/response-handler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
-const { MONGO_URL } = require('./utils/constants');
+const { NODE_ENV, MONGO_URL, MONGO_URL_DEV } = require('./utils/constants');
 
 const app = express();
 
@@ -18,6 +18,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(corsAllow);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 
 app.use(requestLogger); // подключаем логгер запросов
 
@@ -29,7 +35,7 @@ app.use(errorLogger); // подключаем логгер ошибок
 app.use(errors());
 app.use(responseHandler);
 
-mongoose.connect(MONGO_URL)
+mongoose.connect(NODE_ENV === 'production' ? MONGO_URL : MONGO_URL_DEV)
   .then(() => {
     console.log('MongoDB connected');
   });
--- utils/constants.js
+++ utils/constants.js
@@ -1,10 +1,11 @@
 // переменные для разработки
-const MONGO_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb';
+const MONGO_URL_DEV = 'mongodb://127.0.0.1:27017/bitfilmsdb';
 const SECRET_KEY_DEV = 'some-secret-key';
 
 const {
   NODE_ENV,
   SECRET_KEY,
+  MONGO_URL,
 } = process.env;
 
 module.exports = {
@@ -12,4 +13,5 @@ module.exports = {
   SECRET_KEY,
   SECRET_KEY_DEV,
   MONGO_URL,
+  MONGO_URL_DEV,
 };
